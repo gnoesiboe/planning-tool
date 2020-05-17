511428d8f405f1bc3d34a317721aa7918afdb088
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-05-17 21:05:03 +0200

    Lock team as project choice is team specific

--- components/addPlanningItem/components/PlanningItemForm.tsx
+++ components/addPlanningItem/components/PlanningItemForm.tsx
@@ -51,6 +51,7 @@ const PlanningItemForm: React.FC<Props> = ({
                         handleFieldChange('teamId', event.target.value)
                     }
                     required
+                    disabled={true}
                 >
                     <option value="" key="none">
                         -- Team --
