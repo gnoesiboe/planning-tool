b9ea8d831127f25a527d4dddf3cc542c0023234b
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-05-18 06:53:45 +0200

    Remove no longer applicant @todos

--- repository/api/projectRepository.ts
+++ repository/api/projectRepository.ts
@@ -3,7 +3,6 @@ import { ProjectsResponseBody } from '../../server/response/types';
 import { Project } from '../../model/planning';
 
 export async function fetchAll(): Promise<Project[]> {
-    // @todo replace host with environment variable
     const response = await fetch(createGetProjectListUrl());
 
     const { projects } = (await response.json()) as ProjectsResponseBody;
--- repository/database/planningItemRepository.ts
+++ repository/database/planningItemRepository.ts
@@ -24,7 +24,6 @@ export async function findAll(): Promise<PlanningItem[]> {
             p.name ASC
     `);
 
-    // @todo move to factory
     return results.map((result) =>
         createPlanningItemFromDatabaseResult(result)
     );
