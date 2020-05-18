f9c4adda4ac3dcff025eca1579fbb86066742ede
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-05-18 22:59:52 +0200

    Show a bit more context

--- components/editPlanningItem/EditPlanningItem.tsx
+++ components/editPlanningItem/EditPlanningItem.tsx
@@ -1,20 +1,36 @@
-import { PlanningItem } from '../../model/planning';
+import { PlanningItem, Team } from '../../model/planning';
 import LinkButton from '../primities/button/LinkButton';
 import Octicon, { Pencil } from '@primer/octicons-react';
 import useShowHideModal from '../../hooks/useShowHideModal';
 import Modal from '../primities/modal/Modal';
 import EditPlanningItemForm from './components/EditPlanningItemForm';
+import { Table } from 'react-bootstrap';
 
 type Props = {
     item: PlanningItem;
+    team: Team;
 };
 
-const EditPlanningItem: React.FC<Props> = ({ item }) => {
+const EditPlanningItem: React.FC<Props> = ({ item, team }) => {
     const { show, hide, visible } = useShowHideModal();
 
     if (visible) {
         return (
             <Modal onRequestClose={() => hide()}>
+                <h1>Edit planning item</h1>
+                <Table bordered>
+                    <tbody>
+                        <tr>
+                            <th style={{ width: '30%' }}>Week</th>
+                            <td>{item.week}</td>
+                        </tr>
+                        <tr>
+                            <th>Team</th>
+                            <td>{team.name}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+
                 <EditPlanningItemForm item={item} onDone={() => hide()} />
             </Modal>
         );
