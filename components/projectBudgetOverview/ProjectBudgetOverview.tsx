f34b252c7abb0f0b4253aeec15a2df68abedc445
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-06-06 22:40:43 +0200

    Toggle budget visibility

--- components/projectBudgetOverview/ProjectBudgetOverview.tsx
+++ components/projectBudgetOverview/ProjectBudgetOverview.tsx
@@ -1,9 +1,13 @@
 import useProjectBudgetOverviewItems from './hooks/useProjectBudgetOverviewItems';
 import Item from './components/Item';
+import useShowHideModal from '../../hooks/useShowHideModal';
+import LinkButton from '../primities/button/LinkButton';
 
 type Props = {};
 
 const ProjectBudgetOverview: React.FC<Props> = () => {
+    const { visible, toggle } = useShowHideModal(true);
+
     const { items } = useProjectBudgetOverviewItems();
 
     if (!items) {
@@ -12,21 +16,29 @@ const ProjectBudgetOverview: React.FC<Props> = () => {
 
     return (
         <div className="project-budget-overview">
-            <table className="table table-borderless">
-                <thead>
-                    <tr>
-                        <th>Project</th>
-                        <th>Period</th>
-                        <th>Planned</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map((item) => (
-                        <Item data={item} key={item.id} />
-                    ))}
-                </tbody>
-            </table>
+            <LinkButton
+                onClick={() => toggle()}
+                className="project-budget-overview--toggle-button"
+            >
+                {visible ? 'hide' : 'show budget'}
+            </LinkButton>
+            {visible && (
+                <table className="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th>Project</th>
+                            <th>Period</th>
+                            <th>Planned</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item) => (
+                            <Item data={item} key={item.id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
