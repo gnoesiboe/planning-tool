c8e7fe89c7dfdcd803e763f9422ae821e986d445
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-10-30 10:04:49 +0100

    Refactor: use syntax with les indenting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,28 +7,26 @@ import { ToastContainer } from 'react-toastify';
 import { Navbar, Nav } from 'react-bootstrap';
 import Link from 'next/link';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark" sticky="top">
-                <Navbar.Brand>Freshheads</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Link href="/">
-                        <Nav.Link href="/">Planning</Nav.Link>
-                    </Link>
-                    <Link href="/projects">
-                        <Nav.Link href="/projects">Projects</Nav.Link>
-                    </Link>
-                </Nav>
-            </Navbar>
-            <div className="container-fluid page-content">
-                <PlanningContextProvider>
-                    <Component {...pageProps} />
-                </PlanningContextProvider>
-                <ToastContainer />
-            </div>
-        </>
-    );
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <>
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <Navbar.Brand>Freshheads</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Link href="/">
+                    <Nav.Link href="/">Planning</Nav.Link>
+                </Link>
+                <Link href="/projects">
+                    <Nav.Link href="/projects">Projects</Nav.Link>
+                </Link>
+            </Nav>
+        </Navbar>
+        <div className="container-fluid page-content">
+            <PlanningContextProvider>
+                <Component {...pageProps} />
+            </PlanningContextProvider>
+            <ToastContainer />
+        </div>
+    </>
+);
 
 export default MyApp;
