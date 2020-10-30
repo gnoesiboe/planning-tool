f6ebdbc7a4cc66b19fe13577c07447c19f8415aa
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-10-30 12:13:09 +0100

    Use custom scss module instead of inline styling to style modal

--- components/primities/modal/Modal.tsx
+++ components/primities/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import ReactModal, { Props as ReactModalProps, Styles } from 'react-modal';
 import { ModalBody, ModalTitle, CloseButton } from 'react-bootstrap';
 import ModalHeader from 'react-bootstrap/ModalHeader';
+import styles from './Modal.module.scss';
 
 ReactModal.setAppElement('#__next');
 
@@ -14,37 +15,26 @@ const Modal: React.FC<Props> = ({
     onRequestClose,
     title,
     ...otherProps
-}) => {
-    const customStyles: Styles = {
-        content: {
-            marginLeft: '50%',
-        },
-        overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.1)',
-            zIndex: 2000,
-        },
-    };
-
-    return (
-        <ReactModal
-            {...otherProps}
-            isOpen={true}
-            style={customStyles}
-            onRequestClose={onRequestClose}
-        >
-            <ModalHeader>
-                {title && <ModalTitle>{title}</ModalTitle>}
-                <CloseButton
-                    onClick={() => {
-                        if (onRequestClose) {
-                            onRequestClose();
-                        }
-                    }}
-                />
-            </ModalHeader>
-            <ModalBody>{children}</ModalBody>
-        </ReactModal>
-    );
-};
+}) => (
+    <ReactModal
+        {...otherProps}
+        isOpen={true}
+        onRequestClose={onRequestClose}
+        className={styles.content}
+        overlayClassName={styles.overlay}
+    >
+        <ModalHeader>
+            {title && <ModalTitle>{title}</ModalTitle>}
+            <CloseButton
+                onClick={() => {
+                    if (onRequestClose) {
+                        onRequestClose();
+                    }
+                }}
+            />
+        </ModalHeader>
+        <ModalBody>{children}</ModalBody>
+    </ReactModal>
+);
 
 export default Modal;
